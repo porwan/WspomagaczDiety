f118940ea06556c785ce007abbea3a8affdd06ef
porwanPrzeciez <porwanski.w@gmail.com>
2016-04-22 22:19:24 +0200

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name wspomagaczDietyApp
+ * @description
+ * # wspomagaczDietyApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('wspomagaczDietyApp', [
+    'ngAnimate'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wspomagaczDietyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wspomagaczDietyApp
+ */
+angular.module('wspomagaczDietyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
